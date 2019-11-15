43ba866e6a10c6081c709a7828b7ac0a804a0e26
Lucas8x <lucas.poring99@gmail.com>
2019-11-15 15:17:41 -0300

    Update stopots-bot.js

--- stopots-bot.js
+++ stopots-bot.js
@@ -1,69 +1,117 @@
 const requestURL = "https://raw.githubusercontent.com/Lucas8x/stopots-bot/master/dicionario.json";
+var dicionario;
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.send();
 request.responseType = 'json';
-request.onload = function () {
-	dicionario = request.response;
-};
-
-function sleep(ms) {
-	return new Promise(resolve => setTimeout(resolve, ms));
+request.onload = () => {
+  dicionario = request.response;
 }
 
-function xpath(elem){
-	return document.evaluate(elem ,document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ).singleNodeValue;
-};
+// Types = 'quick' , 'deny' , 'check' , null
+const validator_type = null;
 
-function randomItem(array){
-	return array[Math.floor(Math.random()*array.length)];
-};
+const randomArrayItem = array => array[Math.floor(Math.random()*array.length)];
 
-function letter(){
-	try {
-		return xpath('//*[@id="letter"]/span/text()');
-	} catch{};
-};
+//const xpath = elem => document.evaluate(elem ,document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+const xpath = path => {
+  var xpath = document.evaluate(path, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+  var temp = [];
+  for (var i = xpath.snapshotLength - 1; i >= 0; i--) {
+    temp.push(xpath.snapshotItem(i));
+  }
+  return temp[0];
+}
 
-function auto_complete(letter){
-	try {
-		for(let i = 0; i < dicionario.length; i++){
-			if(dicionario[i]['letter'] === letter){
-				for(let j = 1; j < 13; j++){
-					try {
-						let campo = xpath(`//*[@class="ct answers" or @class="ct answers up-enter-done"]//label[${j}]/input`).value;
-						if(campo.length === 0){
-							let item_category = xpath(`//*[@class="ct answers" or @class="ct answers up-enter-done"]//label[${j}]/span`).textContent.toLowerCase();
-							if(item_category === 'nome'){
-								item_category = randomItem(["nome feminino", "nome masculino"]);
-							}
-							else if(item_category === 'comida' && dicionario[i]['categories']['comida'].length === 0){
-								item_category = 'comida saudável';
-							};
-							if(dicionario[i]['categories'][item_category].length > 0){
-								let resposta = randomItem(dicionario[i]['categories'][item_category]);
-								xpath(`//*[@class="ct answers" or @class="ct answers up-enter-done"]//label[${j}]/input`).value = resposta;
-							};
-						}
-					} catch {
-						continue;
-					};
-				};
-				break;
-			};
-		};
-	} catch {};
-};
+const getActualLetter = () => {
+  try {
+    return xpath('//*[@id="letter"]/span/text()').textContent.toLowerCase();
+  } catch (e) {}
+}
 
-while (true){
-	try {
-		var button = xpath(`//*[@class="bt-yellow icon-exclamation" 
-												or @class="bt-yellow icon-exclamation shake" 
-												or @class="bt-yellow icon-exclamation disable"]/strong`).textContent.toUpperCase();
-		if(button === 'STOP!'){
-			var actual_letter = letter().textContent.toLowerCase();
-			auto_complete(actual_letter);
-		}
-	} catch {};
-	await sleep(3000);
-};
+const autoComplete = letter => {
+  for(let i = 0; i < dicionario.length; i++) {
+    if(dicionario[i]['letter'] === letter) {
+      for(let j = 1; j < 13; j++) {
+        try {
+          let input_field = xpath(`//*[@class="ct answers" or @class="ct answers up-enter-done"]//label[${j}]/input`).value;
+          if(input_field.length === 0) {
+            let item_category = xpath(`//*[@class="ct answers" or @class="ct answers up-enter-done"]//label[${j}]/span`).textContent.toLowerCase();
+            if(item_category === 'nome') {
+              item_category = randomArrayItem(["nome feminino", "nome masculino"]);
+            }
+            else if(item_category === 'comida' && dicionario[i]['categories']['comida'].length === 0) {
+              item_category = 'comida saudável';
+            }
+            if(dicionario[i]['categories'][item_category].length > 0) {
+              let resposta = randomArrayItem(dicionario[i]['categories'][item_category]);
+              xpath(`//*[@class="ct answers" or @class="ct answers up-enter-done"]//label[${j}]/input`).value = resposta;
+              xpath(`//*[@class="ct answers" or @class="ct answers up-enter-done"]//label[${j}]/input`).innerHTML = resposta;
+              xpath(`//*[@class="ct answers" or @class="ct answers up-enter-done"]//label[${j}]/input`).innerText = resposta;
+              xpath(`//*[@class="ct answers" or @class="ct answers up-enter-done"]//label[${j}]/input`).setAttribute('value', resposta);
+            }
+          }
+        } catch (e) {
+          continue;
+        }
+      }
+      break;
+    }
+  }
+}
+
+const validate = (validator_type, actual_letter) => {
+  if(true) {
+    if(validator_type === 'quick') {
+      xpath('//*[@class="bt-yellow icon-exclamation" or @class="bt-yellow icon-exclamation shake"]').click();
+    }
+    else if(validator_type === 'deny') {
+      for(let i=1; i <= 15;i++) {
+        if(xpath(`//*[@class="ct validation up-enter-done"]//*[@class="scrollElements"]/label[${i}]/span`).textContent.toUpperCase() === 'VALIDADO!') {
+          xpath(`//*[@class="ct validation up-enter-done"]//*[@class="scrollElements"]/label[${i}]/div`).click();
+        }
+      }
+      xpath('//*[@class="bt-yellow icon-exclamation" or @class="bt-yellow icon-exclamation shake"]').click();
+    }
+    else if(validator_type === 'check') {
+      let category = xpath('//*[@class="ct validation up-enter-done"]/div/h3').textContent.toLowerCase();
+      for(let i = 0; i < dicionario.length; i++) {
+        if(dicionario[i]['letter'] === actual_letter) {
+          for(let j=1; i <= 15; j++) {
+            try {
+              if(xpath(`//*[@class="ct validation up-enter-done"]//*[@class="scrollElements"]/label[${j}]/span`).textContent.toUpperCase() === 'VALIDADO!') {
+                let category_answer = xpath(`//*[@class="ct validation up-enter-done"]//*[@class="scrollElements"]/label[${j}]/div`);
+                if(category_answer !== 'nome') {
+                  if(!category_answer in dicionario[i]['categories'][category]) {
+                    xpath(`//*[@class="ct validation up-enter-done"]//*[@class="scrollElements"]/label[${j}]/div`).click();
+                  }
+                }
+                else if(!category_answer in dicionario[i]['nome feminino'] || category_answer in dicionario[i]['nome masculino']) {
+                  xpath(`//*[@class="ct validation up-enter-done"]//*[@class="scrollElements"]/label[${j}]/div`).click();
+                  }
+                }
+            } catch (e) {
+              continue;
+            }
+          }
+          break;
+        }
+      }
+      xpath('//*[@class="bt-yellow icon-exclamation" or @class="bt-yellow icon-exclamation shake"]').click();
+    }
+  }
+}
+
+const gameCheat = () => {
+  try {
+    var button = xpath('//*[@class="bt-yellow icon-exclamation" or @class="bt-yellow icon-exclamation shake" or @class="bt-yellow icon-exclamation disable"]/strong').textContent.toUpperCase();
+    if(button === 'STOP!') {
+      autoComplete(getActualLetter());
+    }
+    else if(button === 'AVALIAR' && validator_type !== null) {
+      validate(validator_type, getActualLetter());
+    }
+  } catch (e) {}
+}
+
+setInterval(gameCheat, 5000);
